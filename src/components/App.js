54138344e60bedfe925fc1c9ebc6c3b272630d16
VladMorzhanov <vlad.morzhanov@gmail.com>
2018-02-08 23:13:13 +0200

    works

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,16 @@ import React, {Component} from 'react';
 import styled from 'styled-components'
 
 const AppWrapper = styled.div`
-    
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    input{
+      margin-bottom: 40px;
+      margin-top: -100px;
+    }
 `
 
 class App extends Component {
@@ -11,13 +20,19 @@ class App extends Component {
         this.state = {
             inputValue: ''
         }
+        this.onChange = this.onChange.bind(this)
+    }
+
+    onChange(e) {
+        this.setState({inputValue: e.currentTarget.value})
     }
 
     render() {
         return (
-            <div className="App">
-                <input value={this.state.inputValue} onChange={(e) => this.setState({inputValue: e.target.valid})}/>
-            </div>
+            <AppWrapper className="App">
+                <input onChange={this.onChange}/>
+                <p>Value: {this.state.inputValue}</p>
+            </AppWrapper>
         );
     }
 }
